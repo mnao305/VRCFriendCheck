197cd1bcd48838867c8f56f1b09c16e558c5208a
mnao305 <naomasa305@gmail.com>
2018-09-27 12:40:05 +0900

    バックグラウンドにログインチェック処理を追加

---
+++ src/background.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.baseURL = 'https://api.vrchat.cloud/api/1';
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+var badge;
+
+chrome.alarms.create('check', {periodInMinutes: 5});
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.browserAction.getBadgeText({}, (res) => {badge = res;});
+    if (alarm.name === "check" && badge !== "！") {
+        axios.get('/auth/user').then(() => {
+            chrome.browserAction.setBadgeText({text: ``});
+        }).catch(() => {
+            chrome.browserAction.setBadgeText({text: `！`});
+            chrome.browserAction.setBadgeBackgroundColor({color: '#F00'});
+        });
+    }
+});
